843dd3abea44ecbcda57e42db2bc6fb7f212c125
Christopher Deo <chris@christopherdeo.com>
2023-09-06 14:40:27 -0500

    Range method practice exercises (#2)
    * range method exercises pt 1
    
    * completed FizzBuzz Exercise

---
+++ range_method_practice/fizz_buzz_range_exercise/main.py
@@ -0,0 +1,20 @@
+# Create a program that prints each number between 1 and 100, including both.
+# If the number is divisible by 3, instead of printing the number, it should print "Fizz"
+# If the number is divisible by 5, instead of printing the number, it should print "Buzz"
+# if the number is divisible by both 3 and 5, it should print "FizzBuzz"
+
+#  requirements
+# for loop with range()
+# modulo operator usage
+# conditionals such as if statements
+# print each number or term to the console as appropriate
+
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 ==0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
